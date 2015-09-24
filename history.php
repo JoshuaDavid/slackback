1c0e92d00ec27756a57a23155b668a7e61c9924b
Joshua David <Joshua@Joshua-David.net>
2015-09-24 02:16:17 -0700

    Simple file to view results as a html table, json, or jsonp

---
+++ history.php
@@ -0,0 +1,56 @@
+<?php
+
+$dbfile = 'db/messages.sqlite'; 
+$dbh = new PDO("sqlite:$dbfile");
+
+$query = "select * from messages";
+$stmt = $dbh->prepare($query);
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$messages = array();
+foreach($results as $result) {
+    $messages []= array(
+        'team' => $result['team_domain'],
+        'channel' => $result['channel_name'],
+        'user' => $result['user_name'],
+        'message' => $result['text']
+    );
+}
+
+if(isset($_GET['json'])) {
+    echo json_encode($messages);
+    exit();
+} else if(isset($_GET['jsonp']) && isset($_GET['callback'])) {
+    echo $_GET['callback'] . '(' . json_encode($messages) . ');';
+    exit();
+}
+?><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>LW Slack History</title>
+    </head>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>Team</th>
+                    <th>Channel</th>
+                    <th>User</th>
+                    <th>Message</th>
+                </tr>
+            </thead>
+            <tbody>
+<?php foreach($messages as $message): ?>
+                <tr>
+                    <td><?php echo htmlentities($message['team']); ?></td>
+                    <td><?php echo htmlentities($message['channel']); ?></td>
+                    <td><?php echo htmlentities($message['user']); ?></td>
+                    <td><?php echo htmlentities($message['message']); ?></td>
+                </tr>
+<?php endforeach; ?>
+            </tbody>
+        </table>
+    </body>
+</html>
